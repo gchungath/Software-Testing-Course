d4c1ed20c5c4f35330cc7f0ab451742a33050ba9
gchungath <gregorychungath@gmail.com>
2026-09-05 12:48:19 -0400

    Add commnets to function subtarct

--- src/Subtract.java
+++ src/Subtract.java
@@ -4,6 +4,7 @@ public class Subtract {
     /**
      * Return the difference of two integers.
      */
+    // this is to subtract two numbers and return the result
     public static int subtract(int x, int y) {
         return x - y;
     }
